1910228d7e3ddf76b0a92b4579de9cf8881a5bd2
Matt <matthew.nicklay@gmail.com>
2013-02-17 00:04:33 -0600

    Major decrease in load time for nltk_web app.

--- project/apps/nltk_web/views.py
+++ project/apps/nltk_web/views.py
@@ -1,47 +1,36 @@
 import os
-from django.shortcuts import get_object_or_404, redirect, render_to_response
+from django.shortcuts import render_to_response
 from django.template import RequestContext
+from glob import glob
 from nltk.corpus import PlaintextCorpusReader
 from nltk.text import Text
-from os.path import abspath,dirname
+from os.path import abspath,basename,dirname
 
 corpus_root = os.path.join(abspath(dirname(__file__)), '../../reviews')
 androidcorpus = PlaintextCorpusReader(corpus_root, '.*')
 
 
-def filelist(dir_name, sub_dir, *args):
-    fl = []
-    for file in os.listdir(dir_name):
-        dirfile = os.path.join(dir_name, file)
-        if os.path.isfile(dirfile):
-            if len(args) == 0:
-                fl.append(dirfile)
-            else:
-                if os.path.splitext(dirfile)[1][1:] in args:
-                    fl.append(dirfile)
-        elif os.path.isdir(dirfile) and sub_dir:
-            fl += filelist(dirfile, sub_dir, *args)
-    return fl
+def cleanfile(filepath):
+    return basename(filepath).replace('.txt', '')
 
 
 def androidapp(appid):
-    appid = appid.split('/')[-1]
-    return Text(androidcorpus.words(appid), name=appid.replace('.txt', ''))
-
-texts = [androidapp(i) for i in filelist(corpus_root, False, 'txt')]
+    appid = '%s.txt' % appid
+    return Text(androidcorpus.words(appid), name=appid)
 
 
 def home(request):
-    RC = RequestContext(request, dict(texts=texts))
+    txtfiles = [cleanfile(i) for i in glob('%s/*.txt' % corpus_root)]
+    RC = RequestContext(request, {'texts': txtfiles})
     return render_to_response('home.html', RC)
 
 
 def count(request):
     if request.method == "POST":
-        corpus = texts[int(request.POST['corpus'])]
+        corpus = androidapp(request.POST['corpus'])
         word = request.POST['word']
         context = {
-                   'mpn': unicode(corpus.name),
+                   'mpn': unicode(corpus.name)[:-4],
                    'count': unicode(corpus.count(word)),
                    'word': unicode(word),
                   }
